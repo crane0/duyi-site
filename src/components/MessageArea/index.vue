3f1d6697d9d6b81333a522db207cfe6dce92c98b
crane0 <bhad5683126@163.com>
2023-10-11 23:22:51 +0800

    详情页的评论

---
+++ src/components/MessageArea/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="message-area-container">
+    <!-- 透传 BlogComment 传递的事件 -->
+    <DataForm v-on="$listeners" />
+    <h3>
+      {{ title }}
+      <span>{{ subTitle }}</span>
+    </h3>
+    <DataList :list="list" />
+    <!-- 滚动加载评论时的 loading 状态 -->
+    <div class="loading" v-loading="isListLoading"></div>
+  </div>
+</template>
+
+<script>
+import DataForm from "./DataForm";
+import DataList from "./DataList";
+export default {
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+    subTitle: {
+      type: String,
+      default: "",
+    },
+    list: {
+      type: Array,
+      default: () => [],
+    },
+    isListLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  components: {
+    DataForm,
+    DataList,
+  },
+};
+</script>
+
+<style scoped lang="less">
+.loading {
+  position: relative;
+  height: 100px;
+}
+</style>
